bca90b386898c829915e1ed638da3c2872dce156
mmaninetti <marco.maninettimm@gmail.com>
2024-03-30 20:28:58 +0100

    changes

--- ONLY NUMERICAL FEATURES/CLASSIFICATION/ACCURACY/umap_decomposition.py
+++ ONLY NUMERICAL FEATURES/CLASSIFICATION/ACCURACY/umap_decomposition.py
@@ -47,7 +47,7 @@ benchmark_suite = openml.study.get_suite(SUITE_ID)  # obtain the benchmark suite
 #task_id=361055
 for task_id in benchmark_suite.tasks:  # iterate over all tasks in the benchmark suite
 
-    if task_id<=361064:
+    if task_id<=361065:
         continue
 
     # Set the random seed for reproducibility
@@ -580,7 +580,7 @@ for task_id in benchmark_suite.tasks:  # iterate over all tasks in the benchmark
     print("Accuracy constant prediction: ", accuracy_constant)
 
     # GAM model
-    if (task_id!=361062) and (task_id!=361063) and (task_id!=361065):
+    if (task_id!=361062) and (task_id!=361063) and (task_id!=361065) and (task_id!=361066):
         def gam_model(trial):
 
             n_splines = []
