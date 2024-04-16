cb4e3b602b8336ee092f59a992f967d5c917c758
mmaninetti <marco.maninettimm@gmail.com>
2024-04-16 18:04:19 +0200

    changes

--- ONLY NUMERICAL FEATURES/REGRESSION/RMSE/tests.py
+++ ONLY NUMERICAL FEATURES/REGRESSION/RMSE/tests.py
@@ -1,13 +1,19 @@
 import pandas as pd
 import numpy as np
 import openml
+from sklearn.linear_model import LinearRegression 
+import lightgbm as lgbm
+import optuna
 from scipy.spatial.distance import mahalanobis
+from sklearn.ensemble import RandomForestRegressor
+from engression import engression
 import torch
 from scipy.spatial.distance import mahalanobis
-from scipy.stats import norm
-from sklearn.metrics import mean_squared_error
+from rtdl_revisiting_models import MLP, ResNet, FTTransformer
 import random
 import os
+from pygam import LinearGAM, s, f
+from utils import EarlyStopping, train, train_trans, train_no_early_stopping, train_trans_no_early_stopping
 from torch.utils.data import TensorDataset, DataLoader
 import re
 import shutil
@@ -19,189 +25,200 @@ SUITE_ID = 336 # Regression on numerical features
 #SUITE_ID = 334 # Classification on numerical and categorical features
 benchmark_suite = openml.study.get_suite(SUITE_ID)  # obtain the benchmark suite
 
-task_id=361073
+# task_id=361072
+for task_id in benchmark_suite.tasks:
 
-# Set the random seed for reproducibility
-N_TRIALS=100
-N_SAMPLES=100
-PATIENCE=40
-N_EPOCHS=1000
-GP_ITERATIONS=1000
-BATCH_SIZE=1024
-seed=10
-torch.cuda.manual_seed_all(seed)
-np.random.seed(seed)
-torch.manual_seed(seed)
-torch.cuda.manual_seed_all(seed)
-random.seed(seed)
+    if task_id==361084:
+        continue
 
-print(f"Task {task_id}")
+    if task_id<361077:
+        continue
 
-task = openml.tasks.get_task(task_id)  # download the OpenML task
-dataset = task.get_dataset()
+    else:
 
-X, y, categorical_indicator, attribute_names = dataset.get_data(
-        dataset_format="dataframe", target=dataset.default_target_attribute)
+        # Set the random seed for reproducibility
+        N_TRIALS=100
+        N_SAMPLES=100
+        PATIENCE=40
+        N_EPOCHS=1000
+        GP_ITERATIONS=1000
+        BATCH_SIZE=1024
+        seed=10
+        torch.cuda.manual_seed_all(seed)
+        np.random.seed(seed)
+        torch.manual_seed(seed)
+        torch.cuda.manual_seed_all(seed)
+        random.seed(seed)
 
-if len(X) > 15000:
-    indices = np.random.choice(X.index, size=15000, replace=False)
-    X = X.iloc[indices,]
-    y = y[indices]
+        print(f"Task {task_id}")
 
-# Remove categorical columns with more than 20 unique values and non-categorical columns with less than 10 unique values
-# Remove non-categorical columns with more than 70% of the data in one category from X_clean
-for col in [attribute for attribute, indicator in zip(attribute_names, categorical_indicator) if indicator]:
-    if len(X[col].unique()) > 20:
-        X = X.drop(col, axis=1)
+        CHECKPOINT_PATH = f'CHECKPOINTS/MAHALANOBIS/task_{task_id}.pt'
 
-X_clean=X.copy()
-for col in [attribute for attribute, indicator in zip(attribute_names, categorical_indicator) if not indicator]:
-    if len(X[col].unique()) < 10:
-        X = X.drop(col, axis=1)
-        X_clean = X_clean.drop(col, axis=1)
-    elif X[col].value_counts(normalize=True).max() > 0.7:
-        X_clean = X_clean.drop(col, axis=1)
+        task = openml.tasks.get_task(task_id)  # download the OpenML task
+        dataset = task.get_dataset()
 
-# Find features with absolute correlation > 0.9
-corr_matrix = X_clean.corr().abs()
-upper_tri = corr_matrix.where(np.triu(np.ones(corr_matrix.shape), k=1).astype(bool))
-high_corr_features = [column for column in upper_tri.columns if any(upper_tri[column] > 0.9)]
+        X, y, categorical_indicator, attribute_names = dataset.get_data(
+                dataset_format="dataframe", target=dataset.default_target_attribute)
+        
+        if (task_id==361082) or (task_id==361088):
+            y=np.log(y)
+        
+        if len(X) > 15000:
+            indices = np.random.choice(X.index, size=15000, replace=False)
+            X = X.iloc[indices,]
+            y = y[indices]
 
-# Drop one of the highly correlated features from X_clean
-X_clean = X_clean.drop(high_corr_features, axis=1)
+        # Remove categorical columns with more than 20 unique values and non-categorical columns with less than 10 unique values
+        # Remove non-categorical columns with more than 70% of the data in one category from X_clean
+        for col in [attribute for attribute, indicator in zip(attribute_names, categorical_indicator) if indicator]:
+            if len(X[col].unique()) > 20:
+                X = X.drop(col, axis=1)
 
-# Rename columns to avoid problems with LGBM
-X = X.rename(columns = lambda x:re.sub('[^A-Za-z0-9_]+', '', x))
+        X_clean=X.copy()
+        for col in [attribute for attribute, indicator in zip(attribute_names, categorical_indicator) if not indicator]:
+            if len(X[col].unique()) < 10:
+                X = X.drop(col, axis=1)
+                X_clean = X_clean.drop(col, axis=1)
+            elif X[col].value_counts(normalize=True).max() > 0.7:
+                X_clean = X_clean.drop(col, axis=1)
 
+        # Find features with absolute correlation > 0.9
+        corr_matrix = X_clean.corr().abs()
+        upper_tri = corr_matrix.where(np.triu(np.ones(corr_matrix.shape), k=1).astype(bool))
+        high_corr_features = [column for column in upper_tri.columns if any(upper_tri[column] > 0.9)]
 
-# calculate the mean and covariance matrix of the dataset
-mean = np.mean(X_clean, axis=0)
-cov = np.cov(X_clean.T)
+        # Drop one of the highly correlated features from X_clean
+        X_clean = X_clean.drop(high_corr_features, axis=1)
 
-# calculate the Mahalanobis distance for each data point
-mahalanobis_dist = [mahalanobis(x, mean, np.linalg.inv(cov)) for x in X_clean.values]
-
-mahalanobis_dist=pd.Series(mahalanobis_dist,index=X_clean.index)
-far_index=mahalanobis_dist.index[np.where(mahalanobis_dist>=np.quantile(mahalanobis_dist,0.8))[0]]
-close_index=mahalanobis_dist.index[np.where(mahalanobis_dist<np.quantile(mahalanobis_dist,0.8))[0]]
-
-X_train_clean = X_clean.loc[close_index,:]
-X_train = X.loc[close_index,:]
-X_test = X.loc[far_index,:]
-y_train = y.loc[close_index]
-y_test = y.loc[far_index]
-
-mean = np.mean(X_train_clean, axis=0)
-cov = np.cov(X_train_clean.T)
-
-# calculate the Mahalanobis distance for each data point
-mahalanobis_dist_ = [mahalanobis(x, mean, np.linalg.inv(cov)) for x in X_train_clean.values]
-
-mahalanobis_dist_=pd.Series(mahalanobis_dist_,index=X_train_clean.index)
-far_index_=mahalanobis_dist_.index[np.where(mahalanobis_dist_>=np.quantile(mahalanobis_dist_,0.8))[0]]
-close_index_=mahalanobis_dist_.index[np.where(mahalanobis_dist_<np.quantile(mahalanobis_dist_,0.8))[0]]
-
-X_train_ = X_train.loc[close_index_,:]
-X_val = X_train.loc[far_index_,:]
-y_train_ = y_train.loc[close_index_]
-y_val = y_train.loc[far_index_]
-
-# Standardize the data
-mean_X_train_ = np.mean(X_train_, axis=0)
-std_X_train_ = np.std(X_train_, axis=0)
-X_train_ = (X_train_ - mean_X_train_) / std_X_train_
-X_val = (X_val - mean_X_train_) / std_X_train_
-
-mean_X_train = np.mean(X_train, axis=0)
-std_X_train = np.std(X_train, axis=0)
-X_train = (X_train - mean_X_train) / std_X_train
-X_test = (X_test - mean_X_train) / std_X_train
+        # Rename columns to avoid problems with LGBM
+        X = X.rename(columns = lambda x:re.sub('[^A-Za-z0-9_]+', '', x))
 
 
-# Convert data to PyTorch tensors
-X_train__tensor = torch.tensor(X_train_.values, dtype=torch.float32)
-y_train__tensor = torch.tensor(y_train_.values, dtype=torch.float32)
-X_train_tensor = torch.tensor(X_train.values, dtype=torch.float32)
-y_train_tensor = torch.tensor(y_train.values, dtype=torch.float32)
-X_val_tensor = torch.tensor(X_val.values, dtype=torch.float32)
-y_val_tensor = torch.tensor(y_val.values, dtype=torch.float32)
-X_test_tensor = torch.tensor(X_test.values, dtype=torch.float32)
-y_test_tensor = torch.tensor(y_test.values, dtype=torch.float32)
+        # calculate the mean and covariance matrix of the dataset
+        mean = np.mean(X_clean, axis=0)
+        cov = np.cov(X_clean.T)
 
-# Convert to use GPU if available
-if torch.cuda.is_available():
-    print("Using GPU")
-    X_train__tensor = X_train__tensor.cuda()
-    y_train__tensor = y_train__tensor.cuda()
-    X_train_tensor = X_train_tensor.cuda()
-    y_train_tensor = y_train_tensor.cuda()
-    X_val_tensor = X_val_tensor.cuda()
-    y_val_tensor = y_val_tensor.cuda()
-    X_test_tensor = X_test_tensor.cuda()
-    y_test_tensor = y_test_tensor.cuda()
-else:
-    print("Using CPU")
+        # calculate the Mahalanobis distance for each data point
+        mahalanobis_dist = [mahalanobis(x, mean, np.linalg.inv(cov)) for x in X_clean.values]
 
-# Create flattened versions of the data
-y_val_np = y_val.values.flatten()
-y_test_np = y_test.values.flatten()
+        mahalanobis_dist=pd.Series(mahalanobis_dist,index=X_clean.index)
+        far_index=mahalanobis_dist.index[np.where(mahalanobis_dist>=np.quantile(mahalanobis_dist,0.8))[0]]
+        close_index=mahalanobis_dist.index[np.where(mahalanobis_dist<np.quantile(mahalanobis_dist,0.8))[0]]
 
-# Create TensorDatasets for training and validation sets
-train__dataset = TensorDataset(X_train__tensor, y_train__tensor)
-train_dataset = TensorDataset(X_train_tensor, y_train_tensor)
-val_dataset = TensorDataset(X_val_tensor, y_val_tensor)
-test_dataset = TensorDataset(X_test_tensor, y_test_tensor)
+        X_train_clean = X_clean.loc[close_index,:]
+        X_train = X.loc[close_index,:]
+        X_test = X.loc[far_index,:]
+        y_train = y.loc[close_index]
+        y_test = y.loc[far_index]
 
-# Create DataLoaders for training and validation sets
-train__loader = DataLoader(train__dataset, batch_size=BATCH_SIZE, shuffle=True)
-train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
-val_loader = DataLoader(val_dataset, batch_size=BATCH_SIZE, shuffle=False)
-test_loader = DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=False)
+        mean = np.mean(X_train_clean, axis=0)
+        cov = np.cov(X_train_clean.T)
 
-# Define d_out and d_in
-d_out = 1  
-d_in=X_train_.shape[1]
+        # calculate the Mahalanobis distance for each data point
+        mahalanobis_dist_ = [mahalanobis(x, mean, np.linalg.inv(cov)) for x in X_train_clean.values]
 
-#### GP model
-approximations = ["vecchia", "fitc"]
-kernels = ["matern_ard", "gaussian_ard"]
-shapes = [0.5, 1.5, 2.5]
-best_RMSE = float('inf')    
-intercept_train=np.ones(X_train_.shape[0])
-intercept_val=np.ones(X_val.shape[0])
-for approx in approximations:
-    for kernel in kernels:
-        if kernel=="matern_ard":
-            for shape in shapes:
-                gp_model = gpb.GPModel(gp_coords=X_train_, cov_function=kernel, cov_fct_shape=shape, likelihood="gaussian", gp_approx=approx)
-                gp_model.fit(y=y_train_, X=intercept_train, params={"trace": True})
-                pred_resp = gp_model.predict(gp_coords_pred=X_val, X_pred=intercept_val, predict_var=True, predict_response=True)['mu']
-                RMSE_GP = np.sqrt(np.mean((y_val-pred_resp)**2))
-                print("RMSE GP temporary: ", RMSE_GP)
-                if RMSE_GP < best_RMSE:
-                    best_RMSE = RMSE_GP
-                    best_approx = approx
-                    best_kernel = kernel
-                    best_shape = shape
+        mahalanobis_dist_=pd.Series(mahalanobis_dist_,index=X_train_clean.index)
+        far_index_=mahalanobis_dist_.index[np.where(mahalanobis_dist_>=np.quantile(mahalanobis_dist_,0.8))[0]]
+        close_index_=mahalanobis_dist_.index[np.where(mahalanobis_dist_<np.quantile(mahalanobis_dist_,0.8))[0]]
+
+        X_train_ = X_train.loc[close_index_,:]
+        X_val = X_train.loc[far_index_,:]
+        y_train_ = y_train.loc[close_index_]
+        y_val = y_train.loc[far_index_]
+
+        # Standardize the data
+        mean_X_train_ = np.mean(X_train_, axis=0)
+        std_X_train_ = np.std(X_train_, axis=0)
+        X_train_ = (X_train_ - mean_X_train_) / std_X_train_
+        X_val = (X_val - mean_X_train_) / std_X_train_
+
+        mean_X_train = np.mean(X_train, axis=0)
+        std_X_train = np.std(X_train, axis=0)
+        X_train = (X_train - mean_X_train) / std_X_train
+        X_test = (X_test - mean_X_train) / std_X_train
+
+
+        # Convert data to PyTorch tensors
+        X_train__tensor = torch.tensor(X_train_.values, dtype=torch.float32)
+        y_train__tensor = torch.tensor(y_train_.values, dtype=torch.float32)
+        X_train_tensor = torch.tensor(X_train.values, dtype=torch.float32)
+        y_train_tensor = torch.tensor(y_train.values, dtype=torch.float32)
+        X_val_tensor = torch.tensor(X_val.values, dtype=torch.float32)
+        y_val_tensor = torch.tensor(y_val.values, dtype=torch.float32)
+        X_test_tensor = torch.tensor(X_test.values, dtype=torch.float32)
+        y_test_tensor = torch.tensor(y_test.values, dtype=torch.float32)
+
+        # Convert to use GPU if available
+        if torch.cuda.is_available():
+            print("Using GPU")
+            X_train__tensor = X_train__tensor.cuda()
+            y_train__tensor = y_train__tensor.cuda()
+            X_train_tensor = X_train_tensor.cuda()
+            y_train_tensor = y_train_tensor.cuda()
+            X_val_tensor = X_val_tensor.cuda()
+            y_val_tensor = y_val_tensor.cuda()
+            X_test_tensor = X_test_tensor.cuda()
+            y_test_tensor = y_test_tensor.cuda()
         else:
-            gp_model = gpb.GPModel(gp_coords=X_train_, cov_function=kernel, likelihood="gaussian", gp_approx=approx)
-            gp_model.fit(y=y_train_, X=intercept_train, params={"trace": True})
-            pred_resp = gp_model.predict(gp_coords_pred=X_val, X_pred=intercept_val, predict_var=True, predict_response=True)['mu']
-            RMSE_GP = np.sqrt(np.mean((y_val-pred_resp)**2))
-            print("RMSE GP temporary: ", RMSE_GP)
-            if RMSE_GP < best_RMSE:
-                best_RMSE = RMSE_GP
-                best_approx = approx
-                best_kernel = kernel
-                best_shape = None
+            print("Using CPU")
 
-intercept_train=np.ones(X_train.shape[0])
-intercept_test=np.ones(X_test.shape[0])
-if best_kernel=="matern_ard":
-    gp_model = gpb.GPModel(gp_coords=X_train, cov_function=best_kernel, cov_fct_shape=best_shape, likelihood="gaussian", gp_approx=best_approx)
-else:
-    gp_model = gpb.GPModel(gp_coords=X_train, cov_function=best_kernel, likelihood="gaussian", gp_approx=best_approx)
+        # Create flattened versions of the data
+        y_val_np = y_val.values.flatten()
+        y_test_np = y_test.values.flatten()
 
-gp_model.fit(y=y_train, X=intercept_train, params={"trace": True})
-pred_resp = gp_model.predict(gp_coords_pred=X_test, X_pred=intercept_test, predict_var=True, predict_response=True)['mu']
-RMSE_GP = np.sqrt(np.mean((y_test-pred_resp)**2))  
+        # Create TensorDatasets for training and validation sets
+        train__dataset = TensorDataset(X_train__tensor, y_train__tensor)
+        train_dataset = TensorDataset(X_train_tensor, y_train_tensor)
+        val_dataset = TensorDataset(X_val_tensor, y_val_tensor)
+        test_dataset = TensorDataset(X_test_tensor, y_test_tensor)
+
+        # Create DataLoaders for training and validation sets
+        train__loader = DataLoader(train__dataset, batch_size=BATCH_SIZE, shuffle=True)
+        train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
+        val_loader = DataLoader(val_dataset, batch_size=BATCH_SIZE, shuffle=False)
+        test_loader = DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=False)
+
+        # Define d_out and d_in
+        d_out = 1  
+        d_in=X_train_.shape[1]
+
+        #### GAM model
+        def gam_model(trial):
+
+            # Define the search space for n_splines, lam, and spline_order
+            n_splines=trial.suggest_int('n_splines', 10, 100)
+            lam=trial.suggest_float('lam', 1e-3, 1e3, log=True)
+            spline_order=trial.suggest_int('spline_order', 1, 5)
+            
+            # Create and train the model
+            gam = LinearGAM(n_splines=n_splines, spline_order=spline_order, lam=lam).fit(X_train_, y_train_)
+
+            # Predict on the validation set and calculate the RMSE
+            y_val_hat_gam = gam.predict(X_val)
+            RMSE_gam = np.sqrt(np.mean((y_val - y_val_hat_gam) ** 2))
+
+            return RMSE_gam
+
+        # Create the sampler and study
+        sampler_gam = optuna.samplers.TPESampler(seed=seed)
+        study_gam = optuna.create_study(sampler=sampler_gam, direction='minimize')
+
+        # Optimize the model
+        study_gam.optimize(gam_model, n_trials=N_TRIALS)
+
+        # Get the best parameters
+        best_params = study_gam.best_params
+        n_splines=best_params['n_splines']
+        lam=best_params['lam']
+        spline_order=best_params['spline_order']
+
+        final_gam_model = LinearGAM(n_splines=n_splines, spline_order=spline_order, lam=lam)
+
+        # Fit the model
+        final_gam_model.fit(X_train, y_train)
+
+        # Predict on the test set
+        y_test_hat_gam = final_gam_model.predict(X_test)
+        # Calculate the RMSE
+        RMSE_gam = np.sqrt(np.mean((y_test - y_test_hat_gam) ** 2))
+    print("RMSE GAM: ", RMSE_gam)
