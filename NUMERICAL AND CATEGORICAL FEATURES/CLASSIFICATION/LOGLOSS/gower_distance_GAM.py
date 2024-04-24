c90c6923e1e1677539eb1a0ebe04d069e117989c
mmaninetti <marco.maninettimm@gmail.com>
2024-04-24 23:17:20 +0200

    changes

--- NUMERICAL AND CATEGORICAL FEATURES/CLASSIFICATION/LOGLOSS/gower_distance_GAM.py
+++ NUMERICAL AND CATEGORICAL FEATURES/CLASSIFICATION/LOGLOSS/gower_distance_GAM.py
@@ -35,6 +35,12 @@ benchmark_suite = openml.study.get_suite(SUITE_ID)  # obtain the benchmark suite
 #task_id=361110
 for task_id in benchmark_suite.tasks:  # iterate over all tasks in the suite
 
+    if task_id<361111:
+        continue
+
+    if task_id==361111:
+        continue
+
     # Set the random seed for reproducibility
     N_TRIALS=100
     N_SAMPLES=100
