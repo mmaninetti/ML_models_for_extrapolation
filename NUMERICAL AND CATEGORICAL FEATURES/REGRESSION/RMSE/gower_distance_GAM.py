e09b170e7ca690da4ca92cb1c7c1735ce434f576
mmaninetti <marco.maninettimm@gmail.com>
2024-04-27 18:30:43 +0200

    changes

--- NUMERICAL AND CATEGORICAL FEATURES/REGRESSION/RMSE/gower_distance_GAM.py
+++ NUMERICAL AND CATEGORICAL FEATURES/REGRESSION/RMSE/gower_distance_GAM.py
@@ -39,7 +39,7 @@ for task_id in benchmark_suite.tasks:
     if task_id < 361289:
         continue
 
-    if task_id == 361097 or task_id == 361099 or task_id == 361103 or task_id == 361289:
+    if task_id == 361097 or task_id == 361099 or task_id == 361103 or task_id == 361289 or task_id == 361292:
         continue
 
     # Set the random seed for reproducibility
