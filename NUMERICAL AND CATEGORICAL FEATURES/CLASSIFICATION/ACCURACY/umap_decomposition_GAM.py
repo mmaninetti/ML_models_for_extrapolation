b1e02227a564380b36fcc61883a957850aaff705
mmaninetti <marco.maninettimm@gmail.com>
2024-05-04 05:06:14 +0200

    changes

--- NUMERICAL AND CATEGORICAL FEATURES/CLASSIFICATION/ACCURACY/umap_decomposition_GAM.py
+++ NUMERICAL AND CATEGORICAL FEATURES/CLASSIFICATION/ACCURACY/umap_decomposition_GAM.py
@@ -36,7 +36,10 @@ benchmark_suite = openml.study.get_suite(SUITE_ID)  # obtain the benchmark suite
 #task_id=361110
 for task_id in benchmark_suite.tasks:  # iterate over all tasks in the suite
 
-    if task_id==361110:
+    if task_id < 361113:
+        continue
+    
+    if task_id==361110 or task_id==361113:
         continue
 
     # Set the random seed for reproducibility
