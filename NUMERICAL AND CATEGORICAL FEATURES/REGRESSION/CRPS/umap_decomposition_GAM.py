a58f1eb2e1127ff8497e88b62ffd1575a23ea53a
mmaninetti <marco.maninettimm@gmail.com>
2024-05-08 00:01:11 +0200

    changes

--- NUMERICAL AND CATEGORICAL FEATURES/REGRESSION/CRPS/umap_decomposition_GAM.py
+++ NUMERICAL AND CATEGORICAL FEATURES/REGRESSION/CRPS/umap_decomposition_GAM.py
@@ -39,10 +39,10 @@ benchmark_suite = openml.study.get_suite(SUITE_ID)  # obtain the benchmark suite
 #task_id=361093
 for task_id in benchmark_suite.tasks:
 
-    if task_id < 361096:
+    if task_id < 361097:
         continue
 
-    if task_id == 361096:
+    if task_id == 361096 or task_id == 361097:
         continue
 
     # Set the random seed for reproducibility
